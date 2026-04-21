cfb3f11f13f17e7838cc69786327da618b7f1188
hema-code06 <hemavathi.siva06@gmail.com>
2026-04-21 22:11:20 +0530

    styled upload page clean UI design

--- frontend/src/pages/UploadPage.jsx
+++ frontend/src/pages/UploadPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import "../App.css";
+import "./UploadPage.css";
 
 const UploadPage = ({ onUpload, isLoading }) => {
   const fileInputRef = useRef(null);
